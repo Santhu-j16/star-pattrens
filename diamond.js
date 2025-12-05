0b42450586db4e4da7f8325291e93d2f04a2aef3
Santhosh Kumar J <santhujayanna459@gmail.com>
2025-12-05 18:40:57 +0530

    Update diamond.js

--- diamond.js
+++ diamond.js
@@ -2,12 +2,12 @@ const n = 5;
 let pattern = "";
 
 for (let i = 1; i <= n; i++) {
-  pattern += " ".repeat(n - i) + "*".repeat(2 * i - 1) + "\n";
+  pattern += " ".repeat(n - i) + "*".repeat(2 * i - 1) +"\n";
 }
 
 
-for (let i = n - 1; i >= 1; i--) {
-  pattern += " ".repeat(n - i) + "*".repeat(2 * i - 1) + "\n";
+for (let i = n -1; i >= 1; i--) {
+  pattern += " ".repeat(n - i) +"*".repeat(2 * i - 1) + "\n";
 }
 
 console.log(pattern);
