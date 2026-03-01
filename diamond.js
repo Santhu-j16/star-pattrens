2b8c54d76488ba39929ad5a2edf7ac25bd532b5c
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-03-01 12:22:12 +0530

    Fix formatting of diamond pattern generation

--- diamond.js
+++ diamond.js
@@ -2,7 +2,7 @@ const n =8;
 let pattern = " ";
   
 for (let i = 1; i <= n; i++) {
-  pattern += " ".repeat(n - i) + "*".repeat(2 * i - 1) +"\n";
+  pattern += " ".repeat(n - i) + "*".repeat(2 * i - 1) +"\n"; 
 }
 
 
